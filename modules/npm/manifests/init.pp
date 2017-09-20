b634aad9eb9a7c72f4685b75fd71eda108c72208
Bronson Quick <bronson@bronsonquick.com.au>
2017-09-20 08:07:26 +0300

    Add in quotes

--- modules/npm/manifests/init.pp
+++ modules/npm/manifests/init.pp
@@ -1,7 +1,7 @@
 # Install npm
 class npm (
 	$config,
-	$path = /vagrant/extensions/npm,
+	$path = '/vagrant/extensions/npm',
 ) {
 	exec { 'install npm':
 		path    => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
