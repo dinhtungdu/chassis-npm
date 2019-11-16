273bb26b8a88a0704980e4c81564cc1cb626022e
Bronson Quick <bronson@bronsonquick.com.au>
2019-11-16 13:43:58 +1000

    Add disabled extension support

--- modules/npm/manifests/init.pp
+++ modules/npm/manifests/init.pp
@@ -3,15 +3,22 @@ class npm (
 	$config,
 	$path = '/vagrant/extensions/npm',
 ) {
-	exec { 'install npm':
-		path    => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-		command => 'npm install -g npm@latest',
-		require => [ Class['nodejs'] ],
-		unless  => 'which npm',
-	}
+	if ( ! empty($config[disabled_extensions]) and 'chassis/npm' in $config[disabled_extensions] ) {
+		exec { 'uninstall npm':
+			path    => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
+			command => 'npm uninstall npm -g'
+		}
+	} else {
+		exec { 'install npm':
+			path    => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
+			command => 'npm install -g npm@latest',
+			require => [ Class['nodejs'] ],
+			unless  => 'which npm',
+		}
 
-	if ( $config[npm] and $config[npm][paths]) {
-		install_npm { $config[npm][paths]: }
+		if ( $config[npm] and $config[npm][paths]) {
+			install_npm { $config[npm][paths]: }
+		}
 	}
 }
 
